ec9d2b360f565e387deaaa0129ba5697d60eed62
saravanakkumar22 <saravanansankar163@gmail.com>
2024-07-07 20:37:48 +0530

    realign to register and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,8 @@ import { DefaultComponent } from './layouts/default/default.component';
 
 
 const routes: Routes = [
-  {path:"", component:RegisterComponent},
-  {path:"login", component:LoginComponent},
+  {path:"login", component:RegisterComponent},
+  {path:"", component:LoginComponent},
   {path:"home", component:DefaultComponent,
   children:[
     {path:"dashboard", component:DashboardComponent},
